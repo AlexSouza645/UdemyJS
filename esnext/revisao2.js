cb333e752eea6f9920aafb5fdc5973dfc99e3515
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2020-08-17 17:44:36 -0300

    Update revisao2.js

--- esnext/revisao2.js
+++ esnext/revisao2.js
@@ -6,10 +6,11 @@ console.log(soma(2, 3))
 
 //arrow function this
 const lexico1 = () => console.log(this===exports)
-//this esta associado ao local em que ele foi excrito
-const lexico2 = lexico1.bind({})
-lexico1()
-lexico2()
+//this esta associado ao local em que ele foi escrito
+const lexico2 = lexico1.bind({})// bind aponta para 1 objeto
+lexico1()//aponta p/ this
+lexico2()//aponta p/ exports
+//this aponta p/ globo
 
 //parametro default/ valor padrão
 function log(texto = 'node') {
@@ -17,11 +18,13 @@ function log(texto = 'node') {
 }
 log()
 
-numeros.forEach(n=> total += n)// acrescenta um aditivo para cada elemento da array                                                                                                 
+
 //operador rest ou spread
 function total(...numeros){//=conjunto de numeros
     let total = 0// numeros como array
-    return total
+    
+    numeros.forEach(n=> total += n)// acrescenta um aditivo para cada elemento da array                                                                                                 
+return total
 }
 console.log(total(2,3,4,5))
 
