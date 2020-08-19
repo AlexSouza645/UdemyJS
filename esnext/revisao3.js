2600e8c99a7696b65b4c3d86d7121e6546178595
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2020-08-19 18:22:38 -0300

    revisao 3 js

---
+++ esnext/revisao3.js
@@ -0,0 +1,25 @@
+//ES8 Object.value/ object.entries
+const obj = { a: 1, b: 2, c: 3 }
+console.log(Object.values(obj))//array com valores
+console.log(Object.entries(obj))//matriz = array de array chave , valor
+
+//melhorias na notação literal de objetos
+//facilidade de criar objetos
+const nome = 'Carla'
+const pessoa = {
+    nome,//objeto
+    ola() {//ola: function(){}
+        return 'OI gente'
+    }
+
+}
+console.log(pessoa.nome, pessoa.ola())
+
+//classe internamente vai ser convertida em uma função
+class animal {}
+class cachorro extends animal{// extendds= herança em classes
+    falar(){
+        return 'au au'
+    }
+}
+console.log(new cachorro().falar())
