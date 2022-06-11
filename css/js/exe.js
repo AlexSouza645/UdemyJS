7cb2e0a66ef641232529db85ed6cf5fbdd8104f6
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2022-06-11 16:51:05 -0300

    heranca

---
+++ css/js/exe.js
@@ -0,0 +1,16 @@
+
+function main () {
+   // var depth = parseInt(readLine(),10);
+   var feet = 7-2;
+   var day1= feet;
+    var day2= day1 + feet;  
+    var day3= day2 + feet;
+    for (var i = 0; i < 3; i +=5) {
+        console.log(day1);
+        console.log(day2);
+        console.log(day3);
+       
+    }
+ 
+}
+main();
