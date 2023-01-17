af7bc42f7af79b50d2f6f051523f2008ac2d01ef
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2023-01-17 10:54:03 -0300

    ajax

---
+++ ajax/server.js
@@ -0,0 +1,9 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.get('/teste', (req,res)=> res.send('Ok'))
